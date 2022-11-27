28db83bee5ecb51101ec237bc47ef43ec7f07839
Visinigiri Aditya <adityakumarverified@gmail.com>
2022-11-27 19:09:19 +0530

    Day 1 Completed

---
+++ Day-1/main.py
@@ -0,0 +1,6 @@
+print("Hello World!")      # Priting Hello World
+# Priting Hello World new lines 3 times " \n "
+print("Hello World!\nHello World!\nHello World!")
+print("Hello " + "Aditya")  # Concatenating strings
+name = input("What is Your Name ? ")
+print(name)
