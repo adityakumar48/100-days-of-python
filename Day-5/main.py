6d8f2cdc7c40a46cbdb2bf1c3a76b864f9cdee9f
Visinigiri Aditya <adityakumarverified@gmail.com>
2022-12-01 22:23:31 +0530

    Day-5 Completed

---
+++ Day-5/main.py
@@ -0,0 +1,11 @@
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+print(fruits)
+
+total = 0
+for n in range(0, 101, 2):
+    total += n
+print(total)
